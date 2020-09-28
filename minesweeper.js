7d3d55a79466b9188866f84c268ff424b64c6ea4
lukin-ackroyd <70114119+lukin-ackroyd@users.noreply.github.com>
2020-09-28 18:57:52 +1300

    completed minesweeper challenge

--- minesweeper.js
+++ minesweeper.js
@@ -1,11 +1,35 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
-// var board = 
+var board = {
+    cells: []
+}
+let size = 6
+let difficulty = .4
+
+function createBoard () {
+  for (c = 0; c < size; c++) {
+    for (r = 0; r < size; r++) {
+      board.cells.push({
+        col: c,
+        row: r,
+        hidden: true,
+        isMine: Math.random() < difficulty
+      })
+    }
+  }
+}
 
 function startGame () {
   // Don't remove this function call: it makes the game work!
+  createBoard ()
+  for (c = 0; c < board.cells.length; c++) {
+      countSurroundingMines (board.cells[c])
+  }
+  document.addEventListener ("click", checkForWin)
+  document.addEventListener ("contextmenu", checkForWin)
   lib.initBoard()
+
 }
 
 // Define this function to look for a win condition:
@@ -14,6 +38,15 @@ function startGame () {
 // 2. Are all of the mines marked?
 function checkForWin () {
 
+// this needs to say when all mines are marked
+let isWin = true
+for (c = 0; c < board.cells.length; c++) {
+  if ((board.cells[c].isMine && !board.cells[c].isMarked) 
+  || (!board.cells[c].isMine && board.cells[c].hidden)) isWin = false;
+}
+if (isWin) {lib.displayMessage('You win!')}
+
+// this needs to say when all nonmines are not hidden
   // You can use this function call to declare a winner (once you've
   // detected that they've won, that is!)
   //   lib.displayMessage('You win!')
@@ -28,5 +61,13 @@ function checkForWin () {
 // It will return cell objects in an array. You should loop through 
 // them, counting the number of times `cell.isMine` is true.
 function countSurroundingMines (cell) {
+  var surrounding = lib.getSurroundingCells(cell.row, cell.col)
+  let mineCount = 0
+  surrounding.forEach(surCells => {
+    if (surCells.isMine === true) {
+      mineCount++;
+    }
+  });
+  cell.surroundingMines = mineCount
 }
 
